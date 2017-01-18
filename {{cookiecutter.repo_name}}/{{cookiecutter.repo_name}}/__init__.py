9915a436d44fdcd13c6c1203c3577dcf44b511f5
Josip Delic <delijati@googlemail.com>
2017-01-18 09:02:40 +0100

    updated scaffold to 0.3 cornice.ext.swagger

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -6,9 +6,9 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include("cornice")
+    config.include('cornice_swagger')
     config.include("pyramid_chameleon")
     config.add_static_view("static", "static", cache_max_age=3600)
     config.add_route("swagger", "/swagger")
-    config.add_route("swagger_json", "/swagger_json")
     config.scan("{{cookiecutter.repo_name}}.views")
     return config.make_wsgi_app()
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/views.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/views.py
@@ -1,100 +1,72 @@
-""" Cornice services.
-"""
-from cornice import Service
-from cornice.service import get_services
-from cornice_swagger.swagger import generate_swagger_spec
-from pyramid.view import view_config
-from cornice.resource import resource
-from cornice.resource import view
-from cornice.validators import colander_validator
-
 import colander
 
-
-USERS = {1: {"name": "gawel"}, 2: {"name": "tarek"}}
-
-
-class Integers(colander.SequenceSchema):
-    integer = colander.SchemaNode(colander.Integer())
-
-
-class Body(colander.MappingSchema):
-    # {{cookiecutter.repo_name}} and bar are required, baz is optional
-    {{cookiecutter.repo_name}} = colander.SchemaNode(colander.String())
-    ipsum = colander.SchemaNode(colander.Integer(), missing=1,
-                                validator=colander.Range(0, 3))
-    integers = Integers(missing=())
+from cornice import Service
+from cornice.service import get_services
+from cornice.validators import colander_body_validator
+from cornice_swagger.swagger import CorniceSwagger
+from pyramid.view import view_config
 
 
-class Query(colander.MappingSchema):
-    yeah = colander.SchemaNode(colander.String())
-    mau = colander.SchemaNode(colander.String())
+_VALUES = {}
 
 
-class RequestSchema(colander.MappingSchema):
-    body = Body(description="Defines a cornice body schema")
-    querystring = Query()
+# Create a simple service that will store and retrieve values
+values = Service(name="foo",
+                 path="/values/{value}",
+                 description="Cornice Demo")
 
 
-hello = Service(name="hello",
-                path="/{hans}",
-                description="Complex app")
-person = Service(name="person",
-                 path="/{pers_id}/bla/{car_id}",
-                 description="Complex ** 2")
+# Create a body schema for our requests
+class BodySchema(colander.MappingSchema):
+    value = colander.SchemaNode(colander.String(),
+                                description="My precious value")
 
 
-@hello.post(validators=(colander_validator, ), schema=RequestSchema())
-def post_info(request):
-    """Returns Hello in JSON."""
-    return {"Hello": "World"}
+# Create a response schema for our 200 responses
+class OkResponseSchema(colander.MappingSchema):
+    body = BodySchema()
 
 
-@resource(collection_path="/users", path="/users/{id}",
-          name="user_service")
-class User(object):
+# Aggregate the response schemas for get requests
+response_schemas = {
+    "200": OkResponseSchema(description="Return value")
+}
 
-    def __init__(self, request, context=None):
-        self.request = request
-        self.context = context
 
-    def collection_get(self):
-        return {"users": list(USERS.keys())}
+# Create our cornice service views
+class MyValueApi(object):
+    """My precious API."""
 
-    @view(renderer="json")
-    def get(self):
-        return USERS.get(int(self.request.matchdict["id"]))
+    @values.get(response_schemas=response_schemas)
+    def get_value(request):
+        """Returns the value."""
+        key = request.matchdict["value"]
+        return {"value": _VALUES.get(key)}
 
-    @view(renderer="json", content_type="application/json",
-          validators=(colander_validator, ),
-          schema=RequestSchema())
-    def collection_post(self):
-        return {"test": "yeah"}
+    @values.put(validators=(colander_body_validator, ),
+                schema=BodySchema(),
+                response_schemas=response_schemas)
+    def set_value(request):
+        """Set the value and returns *True* or *False*."""
 
-    def patch(self):
-        return {"test": "yeah"}
+        key = request.matchdict["value"]
+        _VALUES[key] = request.json_body
+        return _VALUES.get["key"]
 
-    def collection_patch(self):
-        return {"test": "yeah"}
 
-    def put(self):
-        return dict(type=repr(self.context))
+# Create a service to serve our OpenAPI spec
+swagger = Service(name="OpenAPI",
+                  path="/__api__",
+                  description="OpenAPI documentation")
+
+
+@swagger.get()
+def openAPI_spec(request):
+    my_generator = CorniceSwagger(get_services())
+    my_spec = my_generator("MyAPI", "1.0.0")
+    return my_spec
 
 
 @view_config(route_name="swagger", renderer="templates/index.pt")
-def swagger(request):
+def swagger_ui(request):
     return {"request": request}
-
-
-@view_config(route_name="swagger_json", renderer="json")
-def swagger_json(request):
-    info = {"title": "Joes API", "version": "0.1", "contact": {
-            "name": "Joe Smith",
-            "email": "joe.cool@swagger.com"}
-            }
-    base_path = "/"
-    services = get_services()
-    spec = generate_swagger_spec(services, info["title"],
-                                 info["version"], info=info,
-                                 base_path=base_path, head=False)
-    return spec
